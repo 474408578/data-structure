3c672b70756447955aaf64049c22af386af1f7a3
Xiangsong Chen （陈相嵩）（NOC） <xschen@trip.com>
2020-02-22 16:24:46 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import tools.Timetool;
+
+/***
+ * 斐波那契数列
+ * 0 1 2 3 4 5 6 7
+ * 0 1 1 2 3 5 8 13
+ *
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        Timetool.check("fibonacci", () -> {
+                System.out.println(fibonacci(42));
+        });
+
+        Timetool.check("fibonacci", () -> {
+                System.out.println(fibonacci2(42));
+        });
+    }
+
+    // 使用递归
+    public static int fibonacci(int n){
+        if (n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    // 不使用递归
+    public static int fibonacci2(int n){
+        if (n<=1){
+            return n;
+        }
+        int first = 0;
+        int second = 1;
+        for (int i=0; i<n-1;i++){
+            int sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
+    }
+}
